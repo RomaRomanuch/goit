99df19f84583e99506c339ae40390a8f434f9aec
Romanuch <generalRDR@gmail.com>
2020-09-26 15:47:50 +0300

    added pnotify

--- js/hw-12countries/src/js/markup.js
+++ js/hw-12countries/src/js/markup.js
@@ -1,8 +1,22 @@
 import countryTemplate from '../countryCard.hbs';
+import countriesListTemplate from '../countylist.hbs';
+import { error } from '@pnotify/core';
+
 const countryDomBlock = document.querySelector('.country');
 
 function updateMarkup(data) {
   const markup = countryTemplate(data);
-  countryDomBlock.insertAdjacentHTML('beforeend', markup);
+  const markupList = countriesListTemplate(data);
+  if (data.length === 1) {
+    countryDomBlock.insertAdjacentHTML('beforeend', markup);
+  } else if (data.length > 1 && data.length <= 10) {
+    countryDomBlock.insertAdjacentHTML('beforeend', markupList);
+  } else {
+    error({
+      title: 'Ошибка',
+      text: 'To many matches found. Please enter more specific query!',
+    });
+  }
 }
+
 export default updateMarkup;
