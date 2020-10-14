50c1b5a743e5fbffb6789caa2e2eeedd996add3b
Romanuch <generalRDR@gmail.com>
2020-10-15 00:04:33 +0300

    Updates

---
+++ precache-manifest.9c4d1511755514ba9643902f2399718c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b64c0ef4d41d743032d6fc46f14bc30",
+    "url": "/goit/index.html"
+  },
+  {
+    "revision": "68a9c4e7c101c83fe959",
+    "url": "/goit/static/css/main.569d9641.chunk.css"
+  },
+  {
+    "revision": "f6d0d9b1f6ce7bee1a65",
+    "url": "/goit/static/js/2.3e43f614.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit/static/js/2.3e43f614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a9c4e7c101c83fe959",
+    "url": "/goit/static/js/main.7b28f1cf.chunk.js"
+  },
+  {
+    "revision": "de9a1f26908403351aa7",
+    "url": "/goit/static/js/runtime-main.87db7298.js"
+  }
+]);
